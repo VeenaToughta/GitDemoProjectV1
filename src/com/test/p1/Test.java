3fe1d32c20637a3bd16a0a2d2ad06269ca3411e3
Veena_Toughta <Veena_Toughta@W10528VP73.blr.apac.dell.com>
2021-12-21 11:49:11 +0530

    My First Commit....

---
+++ src/com/test/p1/Test.java
@@ -0,0 +1,16 @@
+package com.test.p1;
+
+public class Test {
+	
+	public static void main(String args[])
+	{
+		String name="TESTING";
+		
+		if(name=="TESTING") {
+			System.out.println("TEST PASSED");
+		}else {
+			System.out.println("TEST FAILED");
+		}
+	}
+
+}
