9cdee7c6f18c2f056bb6fa6b5dfffc8503c2c861
Veena_Toughta <Veena_Toughta@W10528VP73.blr.apac.dell.com>
2021-12-21 14:42:29 +0530

    Commit new class and also modified the Test.java class

--- src/com/test/p1/Test1.java
+++ src/com/test/p1/Test1.java
@@ -1,12 +1,12 @@
 package com.test.p1;
 
-public class Test {
+public class Test1 {
 	
 	public static void main(String args[])
 	{
-		String name="TESTING ONE";
+		String name="TESTING";
 		
-		if(name=="TESTING ONE") {
+		if(name=="TESTING") {
 			System.out.println("TEST PASSED");
 		}else {
 			System.out.println("TEST FAILED");
